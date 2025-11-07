74b9cee1979d6866be6620ace3d3371c584f084c
Raphael Earl Salayog <rlsalayog@findme.com.ph>
2025-11-07 18:47:45 +0800

    feat: radix ui and lucide react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { EyeOutlined } from "@ant-design/icons";
 import { Map } from "@vis.gl/react-google-maps";
-import { Alert, Button, Spin } from "antd";
+import { Alert, Spin } from "antd";
+import { Eye, X } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useGetAllLandmark } from "../api/hooks/useGetAllLandmark";
 import ClusteredLocationMarkers from "../component/google-maps/ClusteredLocationMarkers";
+import { Button } from "../component/ui/button";
 import { MANILA_POSITION } from "../constant";
 
 const Home = () => {
@@ -48,42 +49,46 @@ const Home = () => {
                             <div className="absolute bottom-[4.4rem] -translate-x-1/2">
                                 <div className="relative w-[330px] bg-[rgba(0,0,0,0.8)] text-sm text-white rounded-md">
                                     {/* Arrow */}
-                                    <div className="h-10 w-[68px] absolute -bottom-10 left-1/2 -translate-x-1/2 rotate-180 bg-[rgba(0,0,0,0.8)] [clip-path:polygon(50%_0%,_0%_100%,_100%_100%)]" />
+                                    <div className="h-8 w-[68px] absolute -bottom-8 left-1/2 -translate-x-1/2 rotate-180 bg-[rgba(0,0,0,0.8)] [clip-path:polygon(50%_0%,_0%_100%,_100%_100%)]" />
 
                                     <div className="max-h-96 !p-5 !space-y-7 overflow-auto">
                                         <div className="text-[15px] font-bold grid grid-cols-2 items-center [&>*:nth-child(even)]:text-right">
                                             <p>Market Block</p>
-                                            <button onClick={close} className="cursor-pointer">
-                                                close
-                                            </button>
+                                            <div className="flex justify-end">
+                                                <Button
+                                                    onClick={close}
+                                                    className="p-0 h-auto w-auto cursor-pointer text-white"
+                                                    variant={"link"}
+                                                >
+                                                    <X strokeWidth={3} />
+                                                </Button>
+                                            </div>
                                         </div>
                                         <div className="text-[13px] grid grid-cols-2 items-center [&>*:nth-child(even)]:text-right gap-y-[5px]">
                                             <p>FLOOR PLAN</p>
-                                            <span>
+                                            <div className="flex justify-end">
                                                 <Button
-                                                    icon={<EyeOutlined />}
                                                     onClick={() => {
                                                         navigate(
                                                             `/floor-plan?id=${e.id}&name=${e.name}`
                                                         );
                                                     }}
-                                                />
-                                            </span>
+                                                    className="p-0 h-auto w-auto cursor-pointer text-white"
+                                                    variant={"link"}
+                                                >
+                                                    <Eye />
+                                                </Button>
+                                            </div>
 
-                                            <p>Id :</p>
-                                            <p>{e.id}</p>
-
-                                            <p>Name :</p>
-                                            <p>{e.name}</p>
-
-                                            <p>Category :</p>
-                                            <p>{e.category}</p>
-
-                                            <p>Longitude :</p>
-                                            <p>{e.longitude}</p>
-
-                                            <p>Latitude :</p>
-                                            <p>{e.latitude}</p>
+                                            {Object.entries(e).map(
+                                                ([key, value]) =>
+                                                    key !== "__typename" && (
+                                                        <>
+                                                            <p>{key.toUpperCase()}</p>
+                                                            <p className="uppercase">{value}</p>
+                                                        </>
+                                                    )
+                                            )}
                                         </div>
                                     </div>
                                 </div>
